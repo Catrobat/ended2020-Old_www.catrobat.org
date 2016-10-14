5ddaace8df8551ef7a26d42d45c3e3040e4a4fd6
M. Mueller <matthias.mueller@student.tugraz.at>
2016-10-14 07:36:00 +0200

    Update GA Key

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (isset($_GET["sub"])) {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-XXXXX-Y', 'auto');
+ga('create', 'UA-42270417-8', 'auto');
 ga('send', 'pageview');
 
 function setupScripts() {
